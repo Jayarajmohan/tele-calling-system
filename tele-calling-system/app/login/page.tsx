3ffd86263624caa3c9a013bc6ff50b09b52c2c58
Jayaraj K P <jayaraj.kp@ecsfin.com>
2025-07-08 17:04:35 +0530

    login page demo completed

---
+++ tele-calling-system/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "@/components/login-form"
+export default function LoginPage() {
+  return (
+    <div className=" flex flex-col items-center justify-center p-10 m-10">
+        <LoginForm />
+    </div>
+  )
+}
