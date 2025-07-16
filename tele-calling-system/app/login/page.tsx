9b4aa54bd56cf95820322ff13ffa325e288a6b79
Jayaraj K P <jayaraj.kp@ecsfin.com>
2025-07-16 16:25:01 +0530

    change next to vite

--- tele-calling-system/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import { LoginForm } from "@/components/login-form"
-export default function LoginPage() {
-  return (
-    <div className=" flex flex-col items-center justify-center p-10 m-10">
-        <LoginForm />
-    </div>
-  )
-}
